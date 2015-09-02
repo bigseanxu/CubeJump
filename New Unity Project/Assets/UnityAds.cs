366e6f60fa2f9b236913d1962972d926887c9b85
bigseanxu <bigseanxu@gmail.com>
2015-09-02 11:02:42 +0800

    add unity ads

---
+++ New Unity Project/Assets/UnityAds.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class UnityAds : MonoBehaviour {
+
+	public string GameID = "68793";
+	// Use this for initialization
+	void Start () {
+		Advertisement.Initialize (GameID);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ShowAds ();
+	}
+
+	public void ShowAds() {
+		if(Advertisement.IsReady()){
+			Advertisement.Show();
+		}
+	}
+}
